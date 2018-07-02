f3cff53b057f69df25a65d493fb09553d146333d
mapleB <515186469@qq.com>
2018-07-02 15:56:34 +0800

    file tree右击事件

--- src/FileTree/FileItem.js
+++ src/FileTree/FileItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import FileIcon, { defaultStyles } from 'react-file-icon';
-import { inject, observer } from "mobx-react/index";
+import style from './FileIntem.css'
+import FileIcon, {defaultStyles} from 'react-file-icon';
+import {inject, observer} from "mobx-react/index";
 // import Icon from 'react-native-vector-icons/dist/Feather';
+import {ContextMenu, MenuItem, ContextMenuTrigger} from "react-contextmenu";
 import IconFolder from 'react-icons/lib/fa/folder';
 import IconFolderOpen from 'react-icons/lib/fa/folder-open';
 import IconFile from 'react-icons/lib/fa/file';
+import IconBucket from 'react-icons/lib/fa/bitbucket';
 
 const Container = styled.div`
   
@@ -17,13 +20,25 @@ const FileContainer = styled.div`
   cursor: pointer;
   user-select: none;
   color: #ccc;
-  background: ${props => props.active ? 'rgba(139,191,228,0.2)': 'inherit'};
+  background: ${props => props.active ? 'rgba(139,191,228,0.2)' : 'inherit'};
   padding-left: ${props => 20 + props.depth * 20 + 'px'};
   border-left: 2px solid ${props => props.active ? '#fff' : 'inherit'};
   &:hover {
     background: rgba(108,174,221,0.1);
   }
 `;
+// const ContextMenu = styled.div`
+//   color: #ccc;
+//     background-color: #333;
+//     border-color: #3a3a3a;
+//     opacity: 0.95;
+//     position: absolute;
+//     z-index: 501;
+//     box-shadow: 0 4px 20px 1px rgba(0,0,0,0.3);
+//     border-radius: 4px;
+//     border: 1px solid transparent;
+//     padding: 4px 0;
+// `
 
 // class FileItem1 extends React.Component {
 //   render() {
@@ -37,25 +52,49 @@ const FileContainer = styled.div`
 //   }
 // }
 
+
 const FileItem = inject('store')(
-  observer(({file, store}) => (
-    <Container active={false}>
-      {file.isDir ?
-        <FileContainer depth={file.depth}  onClick={() => {file.toggleDir()}}>
-          {file.expanded ?
-            <IconFolderOpen/>
-            :
-            <IconFolder/>
-          }
-          <span style={{paddingLeft: 5}}>{file.name}</span></FileContainer>
-        :
-        <FileContainer active={file.path === store.view.currentFilePath} depth={file.depth} onDoubleClick={() => {store.openFile(file)}}><IconFile/><span style={{paddingLeft: 5}}>{file.name}</span></FileContainer>
-      }
-      <div style={{display: file.expanded ? 'block' : 'none'}}>
-        {file.children.map(f => <FileItem key={f} file={f}/>)}
+  observer(({file, store}) => {
+
+    const handleClick = (e, data) => {
+      console.log(data.item);
+    }
+
+    return (
+      <div>
+        <ContextMenuTrigger id={file.path} holdToDisplay={1000}>
+          <Container active={false}>
+            {file.isDir ?
+              <FileContainer depth={file.depth} onClick={() => {
+                file.toggleDir()
+              }}>
+                {file.expanded ?
+                  <IconFolderOpen/>
+                  :
+                  <IconFolder/>
+                }
+                <span style={{paddingLeft: 5}}>
+                {file.name}
+                </span></FileContainer>
+              :
+              <FileContainer active={file.path === store.view.currentFilePath} depth={file.depth} onDoubleClick={() => {
+                store.openFile(file)
+              }}><IconFile/><span style={{paddingLeft: 5}}>{file.name}</span></FileContainer>
+            }
+            <div style={{display: file.expanded ? 'block' : 'none'}}>
+              {file.children.map(f => <FileItem key={f} file={f}/>)}
+            </div>
+          </Container>
+        </ContextMenuTrigger>
+        <ContextMenu id={file.path} className='menu'>
+          <MenuItem onClick={handleClick} data={{ item: 'item 1' }} attributes={{className :'menu-item-container'}}><IconFile style={{marginRight: '5px'}}/>添加文件</MenuItem>
+          <MenuItem onClick={this.handleClick} data={{ item: 'item 2' }} attributes={{className :'menu-item-container'}}><IconFolder style={{marginRight: '5px'}}/>添加文件夹</MenuItem>
+          <MenuItem divider />
+          <MenuItem onClick={this.handleClick} data={{ item: 'item 3' }} attributes={{className :'menu-item-container'}}><IconBucket style={{marginRight: '5px'}}/>删除文件</MenuItem>
+        </ContextMenu>
       </div>
-    </Container>
-  ))
+    )
+  })
 )
 
 export default FileItem;
--- src/services/workspace.js
+++ src/services/workspace.js
@@ -2,7 +2,7 @@ import request from '../utils/request';
 import API from "../utils/api";
 
 export function createWorkSpace(data) {
-  return request(API + '/workspace/create', {
+  return request(API + '/workspaces', {
     headers: {
       'Content-Type': 'application/json'
     },
@@ -12,5 +12,5 @@ export function createWorkSpace(data) {
 }
 
 export function getWorkSpace(id) {
-  return request(API + '/workspace/'+id);
+  return request(API + '/workspaces/'+id);
 }
